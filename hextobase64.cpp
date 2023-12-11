a7a992d9e9e768b38d758beaeb4be542e5d8bc1d
U-BSD\414645 <414645@SHS-FYBF6X3.beaverton.k12.or.us>
2023-12-11 10:32:29 -0800

    ABC works, nothing else thouhg

--- hextobase64.cpp
+++ hextobase64.cpp
@@ -14,9 +14,12 @@ int main() {
   char input[LENGTH];
   int intermideate[LENGTH];
   int otherBinay[6*LENGTH];
-    
+  char output[LENGTH];
+  int c = 0;
+   
   for(int a = 0; a < LENGTH; a++) {
     input[a] = ' ';
+    output[a] = ' ';
     intermideate[a] = 0;
   }
   for(int a =0; a < 6*LENGTH; a++) {
@@ -82,6 +85,8 @@ int main() {
   for(int a = 0; a < LENGTH; a++) {
     cout << otherBinay[a];
   }
+  cout << endl;
+  cout << endl;
 
   //turn binary back to char
   for (int a = 0; a < (6 * LENGTH) / 8; a++) {
@@ -90,23 +95,151 @@ int main() {
       //cout << "An error has occured" << endl;
       //exit(1);
       //nvr mind this just means we hit end of string
-      //a = 6 * LENGTH;
+      a = 6 * LENGTH;
     }
-    /*
-    char tmp[6];
+
+    //print what we should have
+    
+    cout << endl;
+    for (int b = 0; b < 8; b++) {
+      cout << otherBinay[8 * a + b];
+    }
+    cout << endl;
+    //*/
+    
+    int tmp = 0;
+    //ignoreing 0 and 1 due to lenght of int restrictions
     //0
     //1
-    tmp[0] = otherBinay[a+2];
-    tmp[1] = otherBinay[a+3];
-    tmp[2] = otherBinay[a+4];
-    tmp[3] = otherBinay[a+5];
-    tmp[4] = otherBinay[a+6];
-    tmp[5] = otherBinay[a+7];
+    tmp += otherBinay[8*a+2] * 100000;
+    tmp += otherBinay[8*a+3] * 10000;
+    tmp += otherBinay[8*a+4] * 1000;
+    tmp += otherBinay[8*a+5] * 100;
+    tmp += otherBinay[8*a+6] * 10;
+    tmp += otherBinay[8*a+7] * 1;
 
-    for (int b = 0; b < 6; b++) {
-      cout << tmp[b];
-      cout << endl;
+    cout << tmp << ", ";
+
+    if (tmp == 000001) {
+      output[c] = 'A';
+      c++;
     }
-    */
+    else if (tmp == 10) {
+      output[c] = 'B';
+      c++;
+    }
+    else if (tmp == 11) {
+      output[c] = 'C';
+      c++;
+    }
+    else if (tmp == 100) {
+      output[c] = 'D';
+      c++;
+    }
+    else if (tmp == 101) {
+      output[c] = 'E';
+      c++;
+    }
+    else if (tmp == 110) {
+      output[c] = 'F';
+      c++;
+    }
+    else if (tmp == 111) {
+      output[c] = 'G';
+      c++;
+    }
+    else if (tmp == 1000) {
+      output[c] = 'H';
+      c++;
+    }
+    else if (tmp == 1001) {
+      output[c] = 'I';
+      c++;
+    }
+    else if (tmp == 1010) {
+      output[c] = 'J';
+      c++;
+    }
+    else if (tmp == 1011) {
+      output[c] = 'K';
+      c++;
+    }
+    else if (tmp == 1100) {
+      output[c] = 'L';
+      c++;
+    }
+    else if (tmp == 1101) {
+      output[c] = 'M';
+      c++;
+    }
+    else if (tmp == 1110) {
+      output[c] = 'N';
+      c++;
+    }
+    else if (tmp == 1111) {
+      output[c] = 'O';
+      c++;
+    }
+    else if (tmp == 10000) {
+      output[c] = 'P';
+      c++;
+    }
+    else if (tmp == 10001) {
+      output[c] = 'Q';
+      c++;
+    }
+    else if (tmp == 10010) {
+      output[c] = 'R';
+      c++;
+    }
+    else if (tmp == 10011) {
+      output[c] = 'S';
+      c++;
+    }
+    else if (tmp == 10100) {
+      output[c] = 'T';
+      c++;
+    }
+    else if (tmp == 10101) {
+      output[c] = 'U';
+      c++;
+    }
+    else if (tmp == 10110) {
+      output[c] = 'V';
+      c++;
+    }
+    else if (tmp == 10111) {
+      output[c] = 'W';
+      c++;
+    }
+    else if (tmp == 11000) {
+      output[c] = 'X';
+      c++;
+    }
+    else if (tmp == 11001) {
+      output[c] = 'Y';
+      c++;
+    }
+    else if (tmp == 11010) {
+      output[c] = 'Z';
+      c++;
+    }
+    else {
+      //should be end character aka drop it
+      //cout << "..." << endl;
+    }
+
+
+    //
+    //cout << c << endl;
+    cout << output[c - 1];
+    cout << endl;
   }
+
+  //cout output
+  cout << endl;
+  for (int a = 0; a < LENGTH; a++) {
+    cout << output[a];
+  }
+  cout << endl;
 }
